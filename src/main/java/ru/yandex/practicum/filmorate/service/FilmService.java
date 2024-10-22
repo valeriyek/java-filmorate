926c79218f9b9032d9134dd95403d226bee8b00c
3agent999 <agentsmith999@yandex.ru>
2024-10-22 14:12:18 +0300

    ТЗ 12 внесение необходимых изменений

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.ResourceNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.genre.GenreStorage;
+import ru.yandex.practicum.filmorate.storage.mpa.MpaStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
@@ -19,11 +22,16 @@ public class FilmService {
     private final FilmStorage filmStorage;
 
     private final UserStorage userStorage;
+    private final MpaStorage mpaStorage;
+    private final GenreStorage genreStorage;
 
     public FilmService(@Qualifier("filmDbStorage") FilmStorage filmStorage,
-                       @Qualifier("userDbStorage") UserStorage userStorage) {
+                       @Qualifier("userDbStorage") UserStorage userStorage, MpaStorage mpaStorage,
+                       GenreStorage genreStorage) {
         this.filmStorage = filmStorage;
         this.userStorage = userStorage;
+        this.mpaStorage = mpaStorage;
+        this.genreStorage = genreStorage;
     }
 
 
@@ -68,6 +76,17 @@ public class FilmService {
 
     public Film addFilm(Film film) {
         validateFilm(film);
+        if (film.getMpa() == null || !mpaStorage.getMpaById(film.getMpa().getId()).isPresent()) {
+            throw new ResourceNotFoundException("MPA с id " + film.getMpa().getId() + " не найден");
+        }
+
+        if (film.getGenres() != null) {
+            for (Genre genre : film.getGenres()) {
+                if (!genreStorage.getGenreById(genre.getId()).isPresent()) {
+                    throw new ResourceNotFoundException("Жанр с id " + genre.getId() + " не найден");
+                }
+            }
+        }
         return filmStorage.addFilm(film);
     }
 
