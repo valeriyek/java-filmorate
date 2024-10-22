e5e1961a23b7a383fd5f945edb8cf7fd61f3ce3b
3agent999 <agentsmith999@yandex.ru>
2024-10-22 20:23:33 +0300

    ТЗ 12 внесение необходимых изменений

--- src/test/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorageTest.java
@@ -15,6 +15,7 @@ import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -50,7 +51,7 @@ public class FilmDbStorageTest {
         film.setReleaseDate(LocalDate.of(2000, 1, 1));
         film.setDuration(120);
         Mpa mpa = new Mpa();
-        mpa.setId(3); // PG-13
+        mpa.setId(3);
         film.setMpa(mpa);
         Genre genre = new Genre();
         genre.setId(1); // Комедия
@@ -61,6 +62,7 @@ public class FilmDbStorageTest {
 
         Film retrievedFilm = filmDbStorage.getFilmById(createdFilm.getId()).orElse(null);
         assertThat(retrievedFilm).isNotNull();
+        assertThat(retrievedFilm.getId()).isEqualTo(1);
         assertThat(retrievedFilm.getName()).isEqualTo("Test Film");
         assertThat(retrievedFilm.getMpa().getName()).isEqualTo("PG-13");
         assertThat(retrievedFilm.getGenres()).hasSize(1);
@@ -94,6 +96,45 @@ public class FilmDbStorageTest {
         List<Film> films = filmDbStorage.getAllFilms();
         assertThat(films).hasSize(2);
         assertThat(films).extracting(Film::getName).containsExactlyInAnyOrder("Film 1", "Film 2");
+    }
+
+    @Test
+    public void testGetPopularFilms() {
+        Mpa mpa = new Mpa();
+        mpa.setId(1);
+
+
+        Film film1 = new Film();
+        film1.setName("Film 1");
+        film1.setDescription("Description 1");
+        film1.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film1.setDuration(120);
+        film1.setMpa(mpa);
+        film1.getLikes().add(1);
+        film1.getLikes().add(2);
+        film1.getLikes().add(3);
+
+        Film film2 = new Film();
+        film2.setName("Film 2");
+        film2.setDescription("Description 2");
+        film2.setReleaseDate(LocalDate.of(2001, 1, 1));
+        film2.setDuration(100);
+        film2.setMpa(mpa);
+        film2.getLikes().add(1);
+        film2.getLikes().add(2);
+
+
+        filmDbStorage.addFilm(film1);
+        filmDbStorage.addFilm(film2);
+
+        List<Film> popularFilms = filmDbStorage.getAllFilms().stream()
+                .sorted((f1, f2) -> Integer.compare(f2.getLikes().size(), f1.getLikes().size()))
+                .limit(1)
+                .collect(Collectors.toList());
+
+
+        assertThat(popularFilms).hasSize(1);
+        assertThat(popularFilms.get(0).getName()).isEqualTo("Film 1");
     }
 
 }
